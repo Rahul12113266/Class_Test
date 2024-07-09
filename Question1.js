481b5cbe1b57f947f6524ec5265b3b605b46dfd3
Rahul12113266 <122507708+Rahul12113266@users.noreply.github.com>
2024-07-09 13:46:13 +0530

    Update Question1.js

--- Question1.js
+++ Question1.js
@@ -4,7 +4,7 @@ const Product = require('../models/Product');
 
 router.get('/products', async (req, res) => {
     try {
-        const minPrice = 500; // Specify your desired minPrice
+        const minPrice = 500; 
         const products = await Product.find({ price: { $gte: minPrice } });
         res.json(products);
     } catch (error) {
